656df8523b3b3e02381b898f45e5f3ddffac4b3e
weijun-sh <huangweijun0322@163.com>
2022-05-17 14:19:01 +0800

    add rsyslog

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # configTools
+
+# rsyslog
+# configRsyslog2Remote
+## client
+### 1, ready log fileslist
+`cd rsyslog`
+`ls /opt/bridge/logs/*log > logFilesList`
+```(The file is an absolute path)```
+### 2, config server
+`vim ./configRsyslog2Remote-client.sh`
+```remoteIPPORT="127.0.0.1:12345"```
+### 3, run
+`./configRsyslog2Remote-client.sh`
---
+++ rsyslog/configRsyslog2Remote-client.sh
@@ -0,0 +1,149 @@
+#!/bin/sh
+remoteIPPORT=""
+logFiles=logFilesList
+
+configFile=/etc/rsyslog.conf
+configFileDefault=/etc/rsyslog.d/50-default.conf
+localName=local0
+
+argsCount=$#
+countLog=0
+restartRsyslog=0
+
+checkIP(){
+	if [ "$remoteIPPORT" = "" ];then
+		echo "Please add remoteIPPORT to $0"
+		exit
+	fi
+}
+
+#check rsyslog version and config file
+checkRsyslog(){
+	rsyslogd -v > /dev/null
+	if [ $? != 0 ];then
+		echo "Please install rsyslog\nExamp: apt install rsyslog"
+		exit
+	fi
+}
+
+usage(){
+	echo "Usage:"
+	echo "\t$0"
+	echo "\t$0 list"
+	echo "\t$0 remove"
+}
+
+checkLog(){
+	if [ -d "$1" ];then
+		echo "\tERR | args1 '$1' is dir, must be file"
+		return 1
+	fi
+	if [ ! -f "$1" ] || [ ! -e "$1" ];then
+		echo "\tERR | logFile '$1' not exist"
+		return 1
+	fi
+	echo "OK"
+}
+checkOp(){
+	if [ "$1" != "list" ] && [ "$1" != "update" ];then
+		echo "ERR | args1 '$1' not support!"
+		usage
+		exit
+	fi
+}
+
+# $#
+checkArgs(){
+	echo "1/3 - check args"
+	if [ $argsCount = 1 ];then
+		checkOp $1
+		echo "\tOK"
+	else
+		if [ $argsCount != 0 ];then
+			usage
+			exit
+		fi
+		echo "\tOK"
+	fi
+}
+
+checkDefault(){
+	echo "3/3 - check $configFileDefault"
+	grep "$localName.none" $configFileDefault > /dev/null
+	if [ $? = 0 ];then
+		echo "\tOK"
+	else
+		grep "^\*.\*" $configFileDefault > /dev/null
+		if [ $? != 0 ];then
+			echo "ERR | $configFileDefault is unrecognizable"
+			exit
+		fi
+		ret=`grep "^\*.\*" $configFileDefault|sed "s/\t/ /g"`
+		retreplace=`echo ${ret% *}`
+		#echo "$retreplace"
+		sed -i "s#$retreplace#$retreplace;$localName.none#g" $configFileDefault
+		echo "\tUpdate | $retreplace;$localName.none"
+	fi
+}
+
+checkConfig(){
+	echo "2/3 - check $configFile"
+	grepStr="# provides kernel logging support and enable non-kernel klog messages"
+	grep "$grepStr" $configFile >/dev/null
+	if [ $? != 0 ];then
+		echo "ERR | $configFileDefault is unrecognizable"
+		exit
+	fi
+	grep "module(load=\"imfile\" PollingInterval" $configFile >/dev/null
+	if [ $? != 0 ];then
+		sed -i "/$grepStr/i\module(load=\"imfile\" PollingInterval=\"10\")" $configFile
+		#module(load="imfile" PollingInterval="10")
+		echo "\tOK  | module(load="imfile" PollingInterval="10")"
+		((restartRsyslog++))
+	fi
+	grep "^$localName.\* @$remoteIPPORT" $configFile >/dev/null
+	if [ $? != 0 ];then
+		sed -i "/$grepStr/i\\$localName.* @$remoteIPPORT" $configFile
+		#local0.* @$remoteIPPORT
+		echo "\tOK  | $localName.* @$remoteIPPORT"
+		((restartRsyslog++))
+	fi
+	while read line
+	do
+		#echo "$line"
+		ret=`checkLog $line`
+		if [ "$ret" != "OK" ];then
+			echo "$ret"
+			continue
+		fi
+		logFile=`realpath $line`
+		grep "type=\"imfile\" File=\"$logFile\"" $configFile >/dev/null
+		if [ $? != 0 ];then
+			tag=`echo ${logFile##*/}`
+			#echo "tag, $tag"
+			sed -i "/$localName.\* @$remoteIPPORT/i\input(type=\"imfile\" File=\"$logFile\" Tag=\"$tag\" Severity=\"info\" Facility=\"$localName\" freshStartTail=\"on\" deleteStateOnFileDelete=\"on\")" $configFile
+			#input(type="imfile" File="/opt/bridge/log/node2.log" Tag="dcrm-node2" Severity="info" Facility="local0" freshStartTail="on" deleteStateOnFileDelete="on")
+			echo "\tOK  | input(type=\"imfile\" File=\"$logFile\" Tag=\"$tag\" Severity=\"info\" Facility=\"$localName\" freshStartTail=\"on\" deleteStateOnFileDelete=\"on\")"
+			((countLog++))
+			((restartRsyslog++))
+		else
+			echo "\tINFO| logFile '$line'($logFile) config exist"
+		fi
+	done < $logFiles
+	echo "\tOK"
+}
+
+# ========== start ==========
+checkRsyslog
+checkIP
+echo "config rsyslog to remote"
+echo
+
+checkArgs $1
+checkConfig
+checkDefault
+echo "\nrsyslog add $countLog file(s)"
+
+if [ $restartRsyslog != 0 ];then
+	systemctl restart rsyslog
+fi
